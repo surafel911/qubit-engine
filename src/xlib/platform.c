7e801970720443c8115fab23a5d6483bfe03681f
Surafel Assefa <surafel911@gmail.com>
2019-04-30 00:20:32 -0400

    Now handles screen sizes.

--- src/xlib/platform.c
+++ src/xlib/platform.c
@@ -9,46 +9,36 @@
 static void
 _qb_platform_xlib_setup(void)
 {
+	Screen* screen;
+
 	_platform.display = XOpenDisplay(NULL);
 	if (_platform.display == NULL) {
-		QB_FATAL("[XLIB] Failed to open display.");
+		QB_FATAL_ERROR("[XLIB] Failed to open display.");
 	}
+	screen = XDefaultScreenOfDisplay(_platform.display);
+
+	_platform.screen_width = XWidthOfScreen(screen);
+	_platform.screen_height = XHeightOfScreen(screen);
 
 	qb_log_info("[XLIB] Connected to display.");
-
-	_platform.screen_id = XDefaultScreen(_platform.display);
-}
-
-static void
-_qb_platform_atoms_setup(void)
-{
-	_platform.atoms.wm_delete_window = XInternAtom(_platform.display,
-		"WM_DELETE_WINDOW", False);
-	_platform.atoms._net_wm_state = XInternAtom(_platform.display,
-		"_NET_WM_STATE", False);
-	_platform.atoms._net_wm_state_fullscreen = XInternAtom(_platform.display,
-		"_NET_WM_STATE_FULLSCREEN", False);
-	_platform.atoms._net_wm_state_maximized_horz = XInternAtom(_platform.display,
-		"_NET_WM_STATE_MAXIMIZED_HORZ", False);
-	_platform.atoms._net_wm_state_maximized_vert = XInternAtom(_platform.display,
-		"_NET_WM_STATE_MAXIMIZED_VERT", False);
-
-	qb_log_info("[XLIB] Atoms created.");
 }
 
 static void
 _qb_platform_glx_setup(void)
 {
-	gladLoaderLoadGLX(_platform.display, _platform.screen_id);
+	int screen_id;
+
+	screen_id = XDefaultScreen(_platform.display);
+	gladLoaderLoadGLX(_platform.display, screen_id);
 	if (GLAD_GLX_VERSION_1_4 == 0) {
-		QB_FATAL("[GLAD] Failed to load GLX.");
+		QB_FATAL_ERROR("[GLAD] Failed to load GLX.");
 	}
 
 	qb_log_info("[GLAD] Loaded GLX.");
 
 	if (GLAD_GLX_ARB_create_context == 0 ||
 		GLAD_GLX_ARB_create_context_profile == 0) {
-		QB_FATAL("[GLAD] GLAD_GLX_ARB_create_context not supported");
+		QB_FATAL_ERROR("[GLAD] GLAD_GLX_ARB_create_context not supported");
 	}
 
 	qb_log_info("[GLAD] GLAD_GLX_ARB_create_context supported.");
@@ -70,7 +60,6 @@ void
 qb_platform_setup(void)
 {
 	_qb_platform_xlib_setup();
-	_qb_platform_atoms_setup();
 	_qb_platform_glx_setup();
 }
 
